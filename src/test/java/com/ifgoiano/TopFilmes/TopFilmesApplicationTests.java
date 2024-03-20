1576facf03512e7c1eafe30b5388a4792cee23ee
Mateus Elias Vieira <mateusifg16@gmail.com>
2024-03-20 07:43:50 -0300

    primeiro commit

---
+++ src/test/java/com/ifgoiano/TopFilmes/TopFilmesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifgoiano.TopFilmes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopFilmesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
